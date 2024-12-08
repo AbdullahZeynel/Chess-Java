76d3e8feda2861cb995f6e3c0e07b93934983ca6
Godfry <147731833+AbdullahZeynel@users.noreply.github.com>
2024-12-08 19:06:51 +0300

    v3.6
    Stale-mate due to insufficient materials added

---
+++ Chess Simulator/Chess Simulator- v3.6/pieces/Piece.java
@@ -0,0 +1,57 @@
+package pieces;
+
+import main.Board;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Piece {
+
+    public int xPos, yPos;
+    public int col, row;
+
+    public boolean isWhite;
+    public String name;
+    public int value;
+
+    public boolean hasMoved = false;  //for pawn and king
+
+    BufferedImage sheet;
+
+
+    {
+        try{
+            sheet = ImageIO.read(new File("res/pieces.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    protected int sheetScale = sheet.getWidth()/6;
+
+    Image sprite;
+    Board board;
+
+    public Piece(Board board){
+
+        this.board = board;
+    }
+
+    public void paint(Graphics g2d){
+
+        g2d.drawImage(sprite, xPos, yPos, null);
+    }
+
+    public boolean isValidMovement(int col, int row){
+        return true;
+    }
+
+    public boolean moveCollidesWithPiece(int col, int row){
+        return false;
+    }
+
+}
