d259d8069672b41c4e6b890837f6976311d925ba
Godfry <147731833+AbdullahZeynel@users.noreply.github.com>
2024-12-14 15:19:43 +0300

    v3.10
    Castle issues fixed + enhancement + clearer readability alongside with some fixes and patches

---
+++ Chess Simulator/Chess Simulator- v3.11/pieces/Knight.java
@@ -0,0 +1,31 @@
+package pieces;
+
+import main.Board;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
+public class Knight extends Piece {
+    public Knight(Board board, int col, int row, boolean isWhite) {
+        super(board);
+        this.col = col;
+        this.row = row;
+        this.xPos = col * board.tile_size;
+        this.yPos = row * board.tile_size;
+
+        this.isWhite = isWhite;
+        this.name = "Knight";
+
+        this.sprite = sheet.getSubimage(3 * sheetScale, isWhite ? 0 : sheetScale, sheetScale, sheetScale).getScaledInstance(board.tile_size, board.tile_size, BufferedImage.SCALE_SMOOTH);
+    }
+
+    @Override
+    public boolean isValidMovement(int col, int row) {
+        return Math.abs(this.col - col) * Math.abs(this.row - row) ==2;
+    }
+
+
+}
