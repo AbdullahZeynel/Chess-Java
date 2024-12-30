e2d9e55629a73e5ceefa5443fed3d88c29af9b3f
Kerem Durgut <146982270+KeremDurgut@users.noreply.github.com>
2024-12-30 22:37:46 +0300

    Update GetDateString.java

--- Chess Simulator/ChessProject v5.7/Main/src/Game/GameEngine/GetDateString.java
+++ Chess Simulator/ChessProject v5.7/Main/src/Game/GameEngine/GetDateString.java
@@ -5,9 +5,13 @@ import java.util.Date;
 
 public class GetDateString {
 
+    // This class will be used for identifying the games by their started time.
+    // When identified, they can be used to analyze the game.
+
     public static String returnDateString(){
         Date date = new Date(); // date when the game starts.
 
+        // Takes the current time in the specified format.
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy hh:mm:ss");
 
         return sdf.format(date);
